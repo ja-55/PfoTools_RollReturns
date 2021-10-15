15ad38cfc26910f05115013890fbfec53d2d3097
Jaison  Abraham <87917798+ja-55@users.noreply.github.com>
2021-10-15 07:09:08 -0500

    Update RollReturns.py

--- RollReturns.py
+++ RollReturns.py
@@ -2,11 +2,11 @@ import pandas as pd
 from yahoo_fin.stock_info import *
 import datetime as dt
 
-tckr = 'SKLZ'
+tckr = 'insert symbol'
 first_date = dt.date(2021,1,1)
 last_date = dt.date(2021,10,14)
-thresh = 0.22
-wdw = 40
+thresh = 'input downside percentage'
+wdw = 'input return window'
 
 srs = get_data(tckr, start_date = first_date, end_date = last_date)
 prc = srs['adjclose']
@@ -17,12 +17,21 @@ prc['shift'] = prc[tckr].shift(wdw)
 prc['shift_rtn'] = (prc[tckr] - prc['shift']) / prc['shift']
 prc['shift_rtn_abs'] = prc['shift_rtn'].map(abs)
 
-prc['shift_rtn_bad'] = prc['shift_rtn_abs'] <= thresh
-prc['shift_rtn_good'] = prc['shift_rtn_abs'] > thresh
+prc['shift_rtn_nom_bad'] = prc['shift_rtn'] <= -thresh
+prc['shift_rtn_nom_good'] = prc['shift_rtn'] > -thresh
+
+prc['shift_rtn_abs_bad'] = prc['shift_rtn_abs'] <= thresh
+prc['shift_rtn_abs_good'] = prc['shift_rtn_abs'] > thresh
 
 
 prc = prc.dropna()
 #prc['shift_rtn'].hist();
 #prc['shift_rtn_abs'].hist();
 
-op = prc.loc[:,['shift_rtn_bad','shift_rtn_good']].sum() / len(prc.index)
+print('Nominal Distribution')
+op_nom = prc.loc[:,['shift_rtn_nom_bad','shift_rtn_nom_good']].sum() / len(prc.index)
+print(op_nom)
+
+print('Absolute Distribution')
+op_abs = prc.loc[:,['shift_rtn_abs_bad','shift_rtn_abs_good']].sum() / len(prc.index)
+print(op_abs)
