7b9ec4d4cfff3564750fce45a22524ffeebfec31
Jaison  Abraham <87917798+ja-55@users.noreply.github.com>
2021-10-15 07:00:24 -0500

    Update RollReturns.py

--- RollReturns.py
+++ RollReturns.py
@@ -5,6 +5,7 @@ import datetime as dt
 tckr = 'SKLZ'
 first_date = dt.date(2021,1,1)
 last_date = dt.date(2021,10,14)
+thresh = 0.22
 wdw = 40
 
 srs = get_data(tckr, start_date = first_date, end_date = last_date)
@@ -14,6 +15,14 @@ prc.columns = [tckr]
 
 prc['shift'] = prc[tckr].shift(wdw)
 prc['shift_rtn'] = (prc[tckr] - prc['shift']) / prc['shift']
+prc['shift_rtn_abs'] = prc['shift_rtn'].map(abs)
+
+prc['shift_rtn_bad'] = prc['shift_rtn_abs'] <= thresh
+prc['shift_rtn_good'] = prc['shift_rtn_abs'] > thresh
+
 
 prc = prc.dropna()
-prc['shift_rtn'].hist();
+#prc['shift_rtn'].hist();
+#prc['shift_rtn_abs'].hist();
+
+op = prc.loc[:,['shift_rtn_bad','shift_rtn_good']].sum() / len(prc.index)
